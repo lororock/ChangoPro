a1f1145c1fb206ecf745cd468a94d596998a5d8f
Cristhian Rosas <cristhianrosas159@hotmail.com>
2023-12-25 10:42:16 -0500

    add text by components

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -1,26 +1,174 @@
+<script setup>
+import { ref } from 'vue';
+
+// Se utiliza ref para crear una referencia al modal
+const modal = ref(null);
+
+// Método para abrir el modal
+const openModal = () => {
+    modal.value.classList.remove('hidden');
+};
+
+// Método para cerrar el modal
+const closeModal = () => {
+    modal.value.classList.add('hidden');
+};
+</script>
+
 <template>
     <section class="pt-10 overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
         <div class="p-8 md:p-12 lg:px-16 lg:py-24">
             <div class="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
                 <h2 class="text-2xl font-bold text-gray-900 md:text-3xl">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                    Transformando Ideas en Experiencias Visuales Inolvidables
                 </h2>
 
                 <p class="hidden text-gray-500 md:mt-4 md:block">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam
-                    sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
-                    quisque ut interdum tincidunt duis.
+                    Nos especializamos en la creación de contenido audiovisual, gestión de campañas, marketing, eventos y
+                    diseño, ofreciendo soluciones integrales que destacan y cautivan.
+
+                    En ChanGo Pro, no simplemente creamos contenido, sino que damos vida a tus visiones. Nuestro equipo
+                    apasionado combina la innovación con la experiencia para ofrecer experiencias visuales impactantes que
+                    resuenan con tu audiencia.
+                </p>
+                <p class="hidden text-gray-500 md:mt-4 md:block">
+                    Explora nuestras creaciones en nuestro portafolio y descubre cómo podemos potenciar tu presencia
+                    digital. En ChanGo Pro, no solo creamos contenido; creamos conexiones, contamos historias y hacemos que
+                    cada proyecto sea memorable.
                 </p>
 
                 <div class="mt-4 md:mt-8">
-                    <a href="#"
-                        class="inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400">
-                        Get Started Today
-                    </a>
+                    <button @click="openModal"
+                        class="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
+                        Nuestro compromiso
+                    </button>
                 </div>
             </div>
         </div>
 
+        <!-- El Modal -->
+        <div ref="modal" class="hidden fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50">
+            <div class="flex items-center justify-center min-h-screen">
+                <div class="bg-white p-8 rounded">
+                    <!-- Contenido del Modal -->
+                    <div
+                        class="divide-y divide-gray-100 rounded-xl border border-gray-100 bg-white max-w-screen-sm mx-auto">
+                        <details class="group p-6 [&_summary::-webkit-details-marker]:hidden" open>
+                            <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                <h2 class="text-lg font-medium">Creación de Contenido Audiovisual</h2>
+
+                                <span class="relative h-5 w-5 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <p class="mt-4 leading-relaxed text-gray-700">
+                                Nuestra creatividad se traduce en videos y contenido audiovisual que cuenta historias
+                                impactantes y cautivadoras
+                            </p>
+                        </details>
+
+                        <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                            <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                <h2 class="text-lg font-medium">Gestión de Campañas y Marketing</h2>
+
+                                <span class="relative h-5 w-5 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <p class="mt-4 leading-relaxed text-gray-700">
+                                Optimizamos tu presencia en línea a través de estrategias de marketing digital y campañas
+                                efectivas que conectan con tu audiencia
+                            </p>
+                        </details>
+
+                        <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                            <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                <h2 class="text-lg font-medium">Eventos Memorables</h2>
+                                <span class="relative h-5 w-5 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <p class="mt-4 leading-relaxed text-gray-700">
+                                Desde la planificación hasta la ejecución, creamos eventos que dejan una impresión duradera,
+                                combinando creatividad y logística impecable
+                            </p>
+                        </details>
+
+                        <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                            <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                <h2 class="text-lg font-medium">Diseño Creativo</h2>
+                                <span class="relative h-5 w-5 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </span>
+                            </summary>
+
+                            <p class="mt-4 leading-relaxed text-gray-700">
+                                Nuestro equipo de diseñadores transforma ideas en visuales atractivos, desde logotipos hasta
+                                materiales de marketing
+                            </p>
+                        </details>
+
+
+                    </div>
+                    <!-- Botón para cerrar el modal -->
+                    <button @click="closeModal"
+                        class="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        Cerrar
+                    </button>
+                </div>
+            </div>
+        </div>
         <img alt="Violin"
             src="https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
             class="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]" />
--- src/components/Informacion.vue
+++ src/components/Informacion.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+// Se utiliza ref para crear una referencia al modal
+const modal = ref(null);
+
+// Método para abrir el modal
+const openModal = () => {
+    modal.value.classList.remove('hidden');
+};
+
+// Método para cerrar el modal
+const closeModal = () => {
+    modal.value.classList.add('hidden');
+};
+</script>
+
 <template>
     <section>
         <div class="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
@@ -15,22 +31,153 @@
 
                     <div class="p-8 sm:p-16 lg:p-24">
                         <h2 class="text-2xl font-bold sm:text-3xl">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.
+                            Transformando Desafíos en Oportunidades
                         </h2>
 
                         <p class="mt-4 text-gray-600">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est
-                            esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam
-                            architecto eius quis quibusdam fugiat dicta.
+                            Acompañamos a empresas que están escribiendo su propia historia de éxito. Desde la creación de
+                            narrativas visuales cautivadoras hasta estrategias de marketing que resonarán con tu audiencia,
+                            estamos aquí para tejer juntos el próximo capítulo de tu historia empresarial.
                         </p>
 
-                        <a href="#"
+                        <p class="mt-4 text-gray-600">
+                            Entendemos que cada campaña es una oportunidad para narrar una historia poderosa. Con nuestra
+                            experiencia en gestión de campañas y creación de contenido impactante, trabajamos contigo para
+                            forjar una conexión auténtica con los votantes. Más que una campaña, es un viaje hacia una
+                            transformación positiva.
+                        </p>
+
+                        <button @click="openModal"
                             class="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
-                            Get in Touch
-                        </a>
+                            Desafíos que Abordamos
+                        </button>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</template>
+
+            <!-- El Modal -->
+            <div ref="modal" class="hidden fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50">
+                <div class="flex items-center justify-center min-h-screen">
+                    <div class="bg-white p-8 rounded">
+                        <!-- Contenido del Modal -->
+                        <div
+                            class="divide-y divide-gray-100 rounded-xl border border-gray-100 bg-white max-w-screen-sm mx-auto">
+                            <details class="group p-6 [&_summary::-webkit-details-marker]:hidden" open>
+                                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                    <h2 class="text-lg font-medium">Visibilidad Limitada</h2>
+
+                                    <span class="relative h-5 w-5 shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </span>
+                                </summary>
+
+                                <p class="mt-4 leading-relaxed text-gray-700">
+                                    Superamos la saturación digital para asegurarnos de que tu marca destaque con
+                                    estrategias de marketing y contenido visual impactantes.
+                                </p>
+                            </details>
+
+                            <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                    <h2 class="text-lg font-medium">Mensaje Confuso</h2>
+
+                                    <span class="relative h-5 w-5 shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </span>
+                                </summary>
+
+                                <p class="mt-4 leading-relaxed text-gray-700">
+                                    Clarificamos y potenciamos tu mensaje, garantizando que llegue de manera clara y
+                                    convincente a tu audiencia objetivo.
+                                </p>
+                            </details>
+
+                            <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                    <h2 class="text-lg font-medium">Eventos Deslucidos</h2>
+
+                                    <span class="relative h-5 w-5 shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </span>
+                                </summary>
+
+                                <p class="mt-4 leading-relaxed text-gray-700">
+                                    Infundimos vida a tus eventos, convirtiéndolos en experiencias memorables que dejan una
+                                    impresión duradera.
+                                </p>
+                            </details>
+
+                            <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                                <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+                                    <h2 class="text-lg font-medium">Diseño Estancado</h2>
+                                    <span class="relative h-5 w-5 shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </span>
+                                </summary>
+
+                                <p class="mt-4 leading-relaxed text-gray-700">
+                                    Revitalizamos la identidad visual de tu marca, transformando ideas en visuales
+                                    atractivos que reflejan su esencia única.
+                                </p>
+                            </details>
+
+                        </div>
+                        <!-- Botón para cerrar el modal -->
+                        <button @click="closeModal"
+                            class="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            Cerrar
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+</section></template>
